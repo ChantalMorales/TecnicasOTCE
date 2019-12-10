1823d4de1853a562f8a19f55f6c33aa40a50c7af
Jonathan Vasquez Vivas <jon2013.p@hotmail.com>
2019-12-10 00:20:38 -0500

    Sistema WPF Login-Ordenes

---
+++ OrdenesTrabajo/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
+
+namespace OrdenesTrabajo
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OrdenesTrabajoEntities context = new OrdenesTrabajoEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            context.Usuario.Load();
+           
+            
+            System.Windows.Data.CollectionViewSource usuarioViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("usuarioViewSource")));
+            // Cargar datos estableciendo la propiedad CollectionViewSource.Source:
+            // usuarioViewSource.Source = [origen de datos genérico]
+        }
+
+        private void contrasenaTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void AddCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+
+            grid1.Visibility = Visibility.Visible;
+            grid2.Visibility = Visibility.Visible;
+
+            // Clear all the text boxes before adding a new customer.  
+            foreach (var child in grid1.Children)
+            {
+                var tb = child as TextBox;
+                if (tb != null)
+                {
+                    tb.Text = "";
+                }
+            }
+            foreach (var child in grid2.Children)
+            {
+                var tb = child as TextBox;
+                if (tb != null)
+                {
+                    tb.Text = "";
+                }
+            }
+        }
+    }
+}
---
+++ OrdenesTrabajo/VentanaOrdenes.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OrdenesTrabajo
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaOrdenes.xaml
+    /// </summary>
+    public partial class VentanaOrdenes : Window
+    {
+        public VentanaOrdenes()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource ordenViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("ordenViewSource")));
+            // Cargar datos estableciendo la propiedad CollectionViewSource.Source:
+            // ordenViewSource.Source = [origen de datos genérico]
+            System.Windows.Data.CollectionViewSource equipoViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("equipoViewSource")));
+            // Cargar datos estableciendo la propiedad CollectionViewSource.Source:
+            // equipoViewSource.Source = [origen de datos genérico]
+        }
+    }
+}
